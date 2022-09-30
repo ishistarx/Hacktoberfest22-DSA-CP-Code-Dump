9fae6f4b5ee40ddebf1c24308e3eb24e4f698a52
Ishita Biswas <96479014+ishistarx@users.noreply.github.com>
2022-10-01 00:14:09 +0530

    convenient

--- Factorial/Factorial.java
+++ Factorial/Factorial.java
@@ -1,14 +1,14 @@
 import java.util.Scanner;
  class Main {
       public static void main(String args[]){
-         int i, factorial=1, number;
-         System.out.println("Enter the number to which you need to find the factorial:");
+         int i, fact=1, number;
+         System.out.println("Enter the number to find the factorial:");
          Scanner sc = new Scanner(System.in);
          number = sc.nextInt();
 
          for(i = 1; i<=number; i++) {
-            factorial = factorial * i;
+            fact = fact * i;
          }
-         System.out.println("Factorial of the given number is:: "+factorial);
+         System.out.println("Factorial of the given number is:: "+fact);
       }
    }
